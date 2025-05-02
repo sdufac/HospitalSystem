4cecd95e6c228910beaa0877199af4c7253119fd
mmando73 <homssword@gmail.com>
2025-05-02 20:11:02 +0200

    personne.html/.js : ajout du CSS

--- client/personne.js
+++ client/personne.js
@@ -1,52 +1,54 @@
 display();
 
-async function display(){
+async function display() {
 	const path = window.location.pathname;
 	const segments = path.split('/');
-	const id = segments[2];  
+	const id = segments[2];
 
-	try{
+	try {
 		const personne = await fetchPers(id);
 
 		const div = document.getElementById("info");
-		if(personne){
+		if (personne) {
 			const title = document.getElementById("title");
 			title.innerText = `${personne.prenomPers} ${personne.nomPers}`;
 
-			div.innerHTML = `Date de naissance: ${personne.dNaisPers}<br>
-					 Numero de téléphone: ${personne.numTelPers}<br>
-					 Adresse: ${personne.adressePers}<br>`;
-			if(personne.specialite){
-				div.innerHTML += `Specialité: ${personne.specialite}`;
-
-				const post = document.getElementById("poste");
-				post.innerText = "Médecin";
-			}else if(personne.datePrisePoste){
-				div.innerHTML += `En poste depuis le: ${personne.datePrisePoste}`;
-
-				const post = document.getElementById("poste");
-				post.innerText = "Infirmier";
-			}
-		}else{
-			div.innerText ="Erreur";
+			// Création d'une liste d'informations avec Bootstrap
+			div.innerHTML = `
+			 <ul class="list-group list-group-flush">
+			   <li class="list-group-item"><strong>Date de naissance :</strong> ${personne.dNaisPers}</li>
+			   <li class="list-group-item"><strong>Numéro de téléphone :</strong> ${personne.numTelPers}</li>
+			   <li class="list-group-item"><strong>Adresse :</strong> ${personne.adressePers}</li>
+			   ${personne.specialite ? `
+				 <li class="list-group-item"><strong>Spécialité :</strong> ${personne.specialite}</li>
+				 <li class="list-group-item"><strong>Poste :</strong> Médecin</li>
+			   ` : ''}
+			   ${personne.datePrisePoste ? `
+				 <li class="list-group-item"><strong>En poste depuis :</strong> ${personne.datePrisePoste}</li>
+				 <li class="list-group-item"><strong>Poste :</strong> Infirmier</li>
+			   ` : ''}
+			 </ul>
+		   `;
+		} else {
+			div.innerHTML = `<div class="alert alert-danger" role="alert">Erreur lors de la récupération des informations.</div>`;
 		}
-	}catch(err){
-		console.error("Erreur display",err);
+	} catch (err) {
+		console.error("Erreur display", err);
 	}
 }
 
-async function fetchPers(id){
-	try{
+async function fetchPers(id) {
+	try {
 		const response = await fetch('/api/personnel/' + id);
 
-		if(!response.ok){
+		if (!response.ok) {
 			throw new Error('Erreur recuperation personne');
 		}
 
 		const patients = await response.json();
 
 		return patients;
-	}catch(error){
+	} catch (error) {
 		console.error("Erreuuur", error);
 	}
 }
