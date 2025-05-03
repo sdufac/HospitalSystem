620045b7dd25be6905bdfe86fc268b391293c175
mmando73 <homssword@gmail.com>
2025-05-03 22:40:49 +0200

    admin.html/.js : ajout du CSS + affichage du service de l'admin

--- client/admin.js
+++ client/admin.js
@@ -1,40 +1,46 @@
 display();
 
-async function display(){
-	try{
+async function display() {
+	try {
 		const admin = await fetchAdmin();
-		//TODO nom prenom service etc
-	}catch(error){
+		if (admin) {
+			const welcome = document.getElementById("welcome");
+			const service = document.getElementById("service");
+
+			welcome.innerText = `Bienvenue ${admin.prenomPers} ${admin.nomPers} !`;
+			service.innerText = `Service : ${admin.service}`;
+		}
+	} catch (error) {
 		console.error("Erreur admin", error);
 	}
 
-	try{
+	try {
 		const rooms = await fetchRooms();
-		if(rooms){
+		if (rooms) {
 			const roomsTable = document.getElementById("rooms");
 			const trRoomName = document.createElement("tr");
 			const trRooms = document.createElement("tr");
 			roomsTable.appendChild(trRoomName);
 			roomsTable.appendChild(trRooms);
 
-			rooms.forEach(r =>{
+			rooms.forEach(r => {
 				const thRoomName = document.createElement("th");
 				const tdRoom = document.createElement("td");
 
 				thRoomName.innerText = r.numChambre;
 				tdRoom.innerText = `${r.nbLitsOccupes}/${r.capacite}`
-				tdRoom.onclick = () =>{
+				tdRoom.onclick = () => {
 					window.location = `/chambre?id=${r.idChambre}`;
 				}
-				tdRoom.style ="cursor: pointer;"
-				
+				tdRoom.style = "cursor: pointer;"
+
 				const today = new Date();
 				const yyyy = today.getFullYear();
 				const mm = String(today.getMonth() + 1).padStart(2, '0'); // Mois: 0-11, donc +1
 				const dd = String(today.getDate()).padStart(2, '0');
 				const datedujour = `${yyyy}-${mm}-${dd}`;
 
-				if(ecartDate(datedujour,r.dateNettoyage) > 2){
+				if (ecartDate(datedujour, r.dateNettoyage) > 2) {
 					tdRoom.className = "rouge";
 				}
 
@@ -42,47 +48,47 @@ async function display(){
 				trRooms.appendChild(tdRoom);
 			});
 		}
-	}catch(error){
+	} catch (error) {
 		console.error("Erreur client patients", error);
 	}
 }
 
-async function fetchRooms(){
-	try{
+async function fetchRooms() {
+	try {
 		const response = await fetch('/api/chambres');
 
-		if(!response.ok){
+		if (!response.ok) {
 			throw new Error('Erreur recuperation données des chambres');
 		}
 
 		const rooms = await response.json();
 
 		return rooms;
-	}catch(error){
+	} catch (error) {
 		console.error("Erreuuur", error);
 	}
 }
 
-async function fetchAdmin(){
-	try{
+async function fetchAdmin() {
+	try {
 		const response = await fetch('/api/getadmin');
-		if(!response.ok){
+		if (!response.ok) {
 			throw new Error('Erreur recuperation admin');
 		}
 
 		const admin = await response.json();
 
 		return admin;
-	}catch(error){
+	} catch (error) {
 		console.error("Erreur admin", error);
 	}
 }
 
-function ecartDate(date1,date2){
+function ecartDate(date1, date2) {
 	const date11 = new Date(date1);
 	const date22 = new Date(date2);
 
-	const diff = Math.abs(date11- date22);
+	const diff = Math.abs(date11 - date22);
 
 	const diffJour = diff / (1000 * 60 * 60 * 24);
 
